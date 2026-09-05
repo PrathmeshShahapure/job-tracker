3f70bd0ba9921b593e30477a10713c489cd20597
PrathmeshShahapure <prathmeshshahapure28@gmail.com>
2026-09-05 16:32:10 +0530

    feat: add application creation

--- server/src/controllers/applicationsController.js
+++ server/src/controllers/applicationsController.js
@@ -1,17 +1,34 @@
 import pool from "../../db/index.js";
-export const getApplications = async (req, res) => { 
-    try {
-          const userID = req.user.userId;
-          console.log(userID);
+export const getApplications = async (req, res) => {
+  try {
+    const userID = req.user.userId;
+    console.log(userID);
 
-          const result = await pool.query(
-            "select * from applications where user_id=$1",
-            [userID],
-          );
+    const result = await pool.query(
+      "select * from applications where user_id=$1",
+      [userID],
+    );
 
-        res.status(200).json({data:result.rows});
-    } catch (error) {
-                 return   res.status(500).json({ message: "Something went Wrong " });
-    }
-  
-}
+    res.status(200).json({ data: result.rows });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went Wrong " });
+  }
+};
+
+export const createApplications = async (req, res) => {
+  try {
+      const userID = req.user.userId;
+      const { company_name,job_title,location,status,applied_at,notes} =req.body;
+    console.log(userID);
+
+    const result = await pool.query(
+      "insert into applications (user_id,company_name,job_title,location,status,applied_at,notes) values($1,$2,$3,$4,$5,$6,$7) returning * ",
+      [userID, company_name, job_title, location, status, applied_at, notes],
+    );
+
+    res.status(201).json({ data: result.rows[0] });
+  } catch (error) {
+      console.log(error)
+    return res.status(500).json({ message: "Something went Wrong " });
+  }
+};
--- server/src/routes/applicationsRouter.js
+++ server/src/routes/applicationsRouter.js
@@ -1,7 +1,10 @@
-import { Router } from "express"
-import { getApplications } from "../controllers/applicationsController.js";
+import { Router } from "express";
+import {
+  getApplications,
+  createApplications,
+} from "../controllers/applicationsController.js";
 const router = Router();
 
-router.get("/",getApplications)
-
-export default router
+router.get("/", getApplications);
+router.post("/", createApplications);
+export default router;

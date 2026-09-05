cae88cf472419018a4653262caa954aaccf283e8
PrathmeshShahapure <prathmeshshahapure28@gmail.com>
2026-09-05 12:43:33 +0530

    feat: add user-specific application access

--- server/src/controllers/applicationsController.js
+++ server/src/controllers/applicationsController.js
@@ -1,5 +1,17 @@
-export const getApplications = (req, res) => { 
-    const userID = req.user.userId;
-    console.log(userID);
-    return res.status(200).json("hi");
+import pool from "../../db/index.js";
+export const getApplications = async (req, res) => { 
+    try {
+          const userID = req.user.userId;
+          console.log(userID);
+
+          const result = await pool.query(
+            "select * from applications where user_id=$1",
+            [userID],
+          );
+
+        res.status(200).json({data:result.rows});
+    } catch (error) {
+                 return   res.status(500).json({ message: "Something went Wrong " });
+    }
+  
 }

0e6b129fb9c49372dda51d291a4085ca742e0c1e
PrathmeshShahapure <prathmeshshahapure28@gmail.com>
2026-09-05 16:59:45 +0530

    feat: add application update

--- server/src/controllers/applicationsController.js
+++ server/src/controllers/applicationsController.js
@@ -17,8 +17,9 @@ export const getApplications = async (req, res) => {
 
 export const createApplications = async (req, res) => {
   try {
-      const userID = req.user.userId;
-      const { company_name,job_title,location,status,applied_at,notes} =req.body;
+    const userID = req.user.userId;
+    const { company_name, job_title, location, status, applied_at, notes } =
+      req.body;
     console.log(userID);
 
     const result = await pool.query(
@@ -28,7 +29,46 @@ export const createApplications = async (req, res) => {
 
     res.status(201).json({ data: result.rows[0] });
   } catch (error) {
-      console.log(error)
+    console.log(error);
+    return res.status(500).json({ message: "Something went Wrong " });
+  }
+};
+
+export const updateApplications = async (req, res) => {
+  try {
+    const { id: appId } = req.params;
+    const userID = req.user.userId;
+    console.log(appId);
+    const { company_name, job_title, location, status, applied_at, notes } =
+      req.body;
+    console.log(userID);
+
+    const isAppIdPresent = await pool.query(
+      "select * from applications where id=$1 and user_id=$2",
+      [appId,userID],
+    );
+    if (isAppIdPresent.rowCount == 0) {
+      return res
+        .status(404)
+        .json({ message: "Unable to find the Application" });
+    }
+    const result = await pool.query(
+      "update applications set company_name=$2,job_title=$3,location=$4,status=$5,applied_at=$6,notes=$7  where id=$8 and user_id=$1 returning * ",
+      [
+        userID,
+        company_name,
+        job_title,
+        location,
+        status,
+        applied_at,
+        notes,
+        appId,
+      ],
+    );
+
+    res.status(200).json({ data: result.rows[0] });
+  } catch (error) {
+    console.log(error);
     return res.status(500).json({ message: "Something went Wrong " });
   }
 };
--- server/src/routes/applicationsRouter.js
+++ server/src/routes/applicationsRouter.js
@@ -2,9 +2,12 @@ import { Router } from "express";
 import {
   getApplications,
   createApplications,
+  updateApplications,
 } from "../controllers/applicationsController.js";
 const router = Router();
 
 router.get("/", getApplications);
 router.post("/", createApplications);
+router.put("/:id", updateApplications);
+
 export default router;
